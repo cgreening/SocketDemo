008850961815fa4516d66687464cb2e73c6a4b63
Chris.Greening <chris@vollow.me>
2015-01-05 15:18:20 +0000

    First commit

--- SocketDemo/ViewController.m
+++ SocketDemo/ViewController.m
@@ -7,17 +7,50 @@
 //
 
 #import "ViewController.h"
+#import <SocketRocket/SRWebSocket.h>
 
-@interface ViewController ()
+@interface ViewController ()<SRWebSocketDelegate, UITextFieldDelegate>
 
+@property(nonatomic, strong) IBOutlet UITextField *messageField;
+@property(nonatomic, strong) IBOutlet UITextView *messages;
+@property(nonatomic, strong) SRWebSocket *webSocket;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.messageField becomeFirstResponder];
+
+    NSURL *wsURL = [NSURL URLWithString:@"wss://demows.herokuapp.com/"];
+    self.webSocket = [[SRWebSocket alloc] initWithURL:wsURL];
+    self.webSocket.delegate = self;
+    [self.webSocket open];
 }
+
+-(BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [self.webSocket send:textField.text];
+    textField.text = @"";
+    return NO;
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message {
+    NSLog(@"Message %@", message);
+    self.messages.text = [NSString stringWithFormat:@"%@\n%@", message, self.messages.text];
+}
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket {
+    NSLog(@"Socket opened");
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    [[[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
+    [[[UIAlertView alloc] initWithTitle:@"Closed" message:reason delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
